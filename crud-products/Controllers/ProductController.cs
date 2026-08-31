1e5ec20edd9821e8da7bc0224d1a9ce4999a7307
Davi Rocha <davirochatz@gmail.com>
2026-08-30 23:33:16 -0300

    feat: Simple controller

---
+++ crud-products/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud_products.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+
+    [HttpPost]
+    public String CreateProduct()
+    {
+        return "Product created";
+    }
+    
+}
